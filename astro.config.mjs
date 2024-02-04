b815fdad8a75d26ed1ddbb40e8ecaf8afd2d39d5
brandon <brandon7.7porcel@gmail.com>
2024-02-04 12:30:17 -0300

    debug: convierto vercel server a serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  })],
-  output: 'server',
-  adapter: vercel()
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  output: "server",
+  adapter: vercelServerless(),
 });
